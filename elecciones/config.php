e36a14b8842014f834f031906976436ecba718a7
sergiokessler <sergiokessler@gmail.com>
2019-04-10 11:55:14 -0300

    charset

--- elecciones/config.php
+++ elecciones/config.php
@@ -29,7 +29,7 @@ $db_charset = 'ISO-8859-1';
 
 $db_dsn = "pgsql:host=$db_host;dbname=$db_dbname";
 
-$config['db']['pg_str'] = 'host=' . $db_host . ' dbname=' . $db_dbname . ' user=' . $db_user . ' password=' . $db_pass;
+$config['db']['pg_str'] = "host=$db_host dbname=$db_dbname user=$db_user password=$db_pass options='--client_encoding=$db_charset'";
 
 
 define('MAX_RECORDS_LIST', 500);
